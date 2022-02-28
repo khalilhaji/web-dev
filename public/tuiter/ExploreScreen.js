028feacc154fdb24a1c9cc94fe91bc9477159c6a
Khalil Haji <khalilalihaji@gmail.com>
2022-02-28 15:19:39 -0500

    fix imports a third time

--- public/tuiter/ExploreScreen.js
+++ public/tuiter/ExploreScreen.js
@@ -1,6 +1,6 @@
-import NavigationSidebar from "./NavigationSidebar/NavigationSidebar.js";
+import NavigationSidebar from "./NavigationSidebar/index.js";
 import ExploreComponent from "./ExploreComponent.js";
-import WhoToFollowList from "./WhoToFollowList/WhoToFollowList.js";
+import WhoToFollowList from "./WhoToFollowList/index.js";
 
 (function ($) {
   $('#wd-explore').append(`
